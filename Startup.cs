1fa9d697e92e5bed847aa74d2a99176f551b690f
nawam727 <nawamdenawakage@gmail.com>
2023-10-09 23:37:07 +0530

    Adding project files

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(adbms_group_project.Startup))]
+namespace adbms_group_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
