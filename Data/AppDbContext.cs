88cd7186457b7ec51a408294909cf0846577ae2c
nawam727 <nawamdenawakage@gmail.com>
2023-10-16 18:42:24 +0530

    Adding Camp CRUD Operation

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using adbms_group_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace adbms_group_project.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Camp> Camps { get; set; }
+    }
+}
